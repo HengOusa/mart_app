e078e2c613e78a084f2b6e76ed31f482fe14c995
HengOusa <hengousaa1@gmail.com>
2026-08-28 10:50:32 +0700

    structure project

--- lib/features/auth/login/screens/login_screen.dart
+++ lib/features/auth/login/screens/login_screen.dart
@@ -1,301 +1,127 @@
 import 'package:ecommerce_app/core/constants/app_colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  bool _rememberMe = false;
-  bool _obscurePassword = true;
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.backgroundWhite,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      backgroundColor: AppColors.primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'Login',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: AppColors.fontgroundWhite,
+            ),
+          ),
+          SizedBox(height: 15),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.8,
+            padding: EdgeInsets.only(left: 30, top: 30, right: 30),
+            decoration: BoxDecoration(
+              color: AppColors.backgroundWhite,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+            ),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 40),
-                // Header Section
-                Center(
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: AppColors.primaryColor.withAlpha(30),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Icon(
-                      Icons.shopping_bag_outlined,
-                      size: 40,
-                      color: AppColors.primaryColor,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 32),
-                // Title
-                Text(
-                  'Welcome Back!',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.fontgroundBlack,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Login to your account to continue shopping',
-                  style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-                ),
-                SizedBox(height: 32),
-                // Email Field
-                Text(
-                  'Email Address',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.fontgroundBlack,
-                  ),
-                ),
-                SizedBox(height: 8),
-                TextField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    hintText: 'Enter your email',
-                    prefixIcon: Icon(
-                      Icons.email_outlined,
-                      color: AppColors.primaryColor,
-                    ),
-                    filled: true,
-                    fillColor: Colors.grey[100],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 16,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                // Password Field
-                Text(
-                  'Password',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.fontgroundBlack,
-                  ),
-                ),
-                SizedBox(height: 8),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: _obscurePassword,
-                  decoration: InputDecoration(
-                    hintText: 'Enter your password',
-                    prefixIcon: Icon(
-                      Icons.lock_outline,
-                      color: AppColors.primaryColor,
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _obscurePassword
-                            ? Icons.visibility_off_outlined
-                            : Icons.visibility_outlined,
-                        color: AppColors.primaryColor,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _obscurePassword = !_obscurePassword;
-                        });
-                      },
-                    ),
-                    filled: true,
-                    fillColor: Colors.grey[100],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 16,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                // Remember Me & Forgot Password
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Checkbox(
-                          value: _rememberMe,
-                          onChanged: (value) {
-                            setState(() {
-                              _rememberMe = value ?? false;
-                            });
-                          },
-                          activeColor: AppColors.primaryColor,
+                Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Please login to your Account',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          'Remember me',
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: AppColors.fontgroundBlack,
+                      ),
+                      SizedBox(height: 10),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'heng@gmail.com',
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(
+                              color: AppColors.fontgroundBlack,
+                              width: 3,
+                            ),
+                          ),
+                          labelStyle: TextStyle(
+                            fontSize: 15,
+                            color: AppColors.primaryColor,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(
+                              color: AppColors.primaryColor,
+                              width: 2,
+                            ),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: AppColors.primaryColor,
                           ),
                         ),
-                      ],
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                          fontSize: 13,
-                          color: AppColors.primaryColor,
-                          fontWeight: FontWeight.w500,
-                        ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 24),
-                // Login Button
-                SizedBox(
-                  width: double.infinity,
-                  height: 56,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          AppColors.primaryColor,
-                          AppColors.primaryColor.withAlpha(200),
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.primaryColor.withAlpha(100),
-                          blurRadius: 16,
-                          offset: Offset(0, 8),
-                        ),
-                      ],
-                    ),
-                    child: Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        onTap: () {},
-                        borderRadius: BorderRadius.circular(12),
-                        child: Center(
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: AppColors.fontgroundWhite,
+                      SizedBox(height: 15),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          labelStyle: TextStyle(
+                            fontSize: 15,
+                            color: AppColors.primaryColor,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: AppColors.primaryColor,
+                          ),
+                          suffixIcon: Icon(CupertinoIcons.eye_slash),
+                          suffixIconColor: AppColors.fontgroundBlack,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(
+                              color: AppColors.primaryColor,
+                              width: 2,
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                // Sign Up Link
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account? ",
-                      style: TextStyle(fontSize: 13, color: Colors.grey[600]),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          fontSize: 13,
-                          color: AppColors.primaryColor,
-                          fontWeight: FontWeight.bold,
+                      SizedBox(height: 10),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: FilledButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all(
+                              AppColors.primaryColor,
+                            ),
+                          ),
+                          child: Text('Login', style: TextStyle(fontSize: 20)),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-                SizedBox(height: 24),
-                // Divider
-                Row(
-                  children: [
-                    Expanded(child: Divider(color: Colors.grey[300])),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Text(
-                        'Or continue with',
-                        style: TextStyle(fontSize: 12, color: Colors.grey[600]),
-                      ),
-                    ),
-                    Expanded(child: Divider(color: Colors.grey[300])),
-                  ],
-                ),
-                SizedBox(height: 20),
-                // Social Login Buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _buildSocialButton(Icons.facebook, 'Facebook'),
-                    SizedBox(width: 16),
-                    _buildSocialButton(Icons.g_translate, 'Google'),
-                    SizedBox(width: 16),
-                    _buildSocialButton(Icons.apple, 'Apple'),
-                  ],
-                ),
-                SizedBox(height: 40),
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSocialButton(IconData icon, String label) {
-    return Container(
-      width: 56,
-      height: 56,
-      decoration: BoxDecoration(
-        color: Colors.grey[100],
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: Colors.grey[300]!),
-      ),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: () {},
-          borderRadius: BorderRadius.circular(12),
-          child: Icon(icon, color: AppColors.fontgroundBlack, size: 24),
-        ),
+        ],
       ),
     );
   }
---
+++ structure_project.md
@@ -0,0 +1,255 @@
+Complete Flutter E-Commerce Project Structure
+
+For a Flutter + GetX + Clean Architecture + Supabase + Responsive Design e-commerce project, I would use the following structure:
+
+lib/
+│
+├── main.dart
+│
+├── app/
+│   ├── app.dart
+│   │
+│   ├── bindings/
+│   │   └── initial_binding.dart
+│   │
+│   ├── routes/
+│   │   ├── app_pages.dart
+│   │   └── app_routes.dart
+│   │
+│   └── theme/
+│       ├── app_colors.dart
+│       ├── app_theme.dart
+│       ├── app_text_styles.dart
+│       └── app_dimensions.dart
+│
+├── core/
+│   │
+│   ├── constants/
+│   │   ├── app_constants.dart
+│   │   ├── supabase_constants.dart
+│   │   └── storage_constants.dart
+│   │
+│   ├── responsive/
+│   │   ├── app_breakpoints.dart
+│   │   ├── responsive.dart
+│   │   ├── responsive_layout.dart
+│   │   └── adaptive_scaffold.dart
+│   │
+│   ├── errors/
+│   │   ├── exceptions.dart
+│   │   └── failures.dart
+│   │
+│   ├── network/
+│   │   └── network_info.dart
+│   │
+│   ├── services/
+│   │   ├── storage_service.dart
+│   │   └── supabase_service.dart
+│   │
+│   ├── utils/
+│   │   ├── validators.dart
+│   │   ├── formatters.dart
+│   │   └── helpers.dart
+│   │
+│   └── widgets/
+│       ├── app_button.dart
+│       ├── app_text_field.dart
+│       ├── app_loader.dart
+│       ├── app_error.dart
+│       │
+│       └── adaptive/
+│           ├── adaptive_grid.dart
+│           ├── adaptive_navigation.dart
+│           └── adaptive_scaffold.dart
+│
+├── data/
+│   │
+│   ├── datasources/
+│   │   │
+│   │   ├── remote/
+│   │   │   ├── auth_remote_datasource.dart
+│   │   │   ├── product_remote_datasource.dart
+│   │   │   ├── category_remote_datasource.dart
+│   │   │   ├── cart_remote_datasource.dart
+│   │   │   └── order_remote_datasource.dart
+│   │   │
+│   │   └── local/
+│   │       ├── auth_local_datasource.dart
+│   │       └── cart_local_datasource.dart
+│   │
+│   ├── models/
+│   │   ├── user_model.dart
+│   │   ├── product_model.dart
+│   │   ├── category_model.dart
+│   │   ├── cart_item_model.dart
+│   │   └── order_model.dart
+│   │
+│   └── repositories/
+│       ├── auth_repository_impl.dart
+│       ├── product_repository_impl.dart
+│       ├── category_repository_impl.dart
+│       ├── cart_repository_impl.dart
+│       └── order_repository_impl.dart
+│
+├── domain/
+│   │
+│   ├── entities/
+│   │   ├── user.dart
+│   │   ├── product.dart
+│   │   ├── category.dart
+│   │   ├── cart_item.dart
+│   │   └── order.dart
+│   │
+│   ├── repositories/
+│   │   ├── auth_repository.dart
+│   │   ├── product_repository.dart
+│   │   ├── category_repository.dart
+│   │   ├── cart_repository.dart
+│   │   └── order_repository.dart
+│   │
+│   └── usecases/
+│       │
+│       ├── auth/
+│       │   ├── login.dart
+│       │   ├── register.dart
+│       │   └── logout.dart
+│       │
+│       ├── products/
+│       │   ├── get_products.dart
+│       │   ├── get_product_by_id.dart
+│       │   └── search_products.dart
+│       │
+│       ├── categories/
+│       │   └── get_categories.dart
+│       │
+│       ├── cart/
+│       │   ├── get_cart.dart
+│       │   ├── add_to_cart.dart
+│       │   ├── update_cart.dart
+│       │   └── remove_from_cart.dart
+│       │
+│       └── orders/
+│           ├── create_order.dart
+│           └── get_orders.dart
+│
+└── features/
+    │
+    ├── splash/
+    │   ├── bindings/
+    │   │   └── splash_binding.dart
+    │   ├── controllers/
+    │   │   └── splash_controller.dart
+    │   ├── views/
+    │   │   └── splash_screen.dart
+    │   └── widgets/
+    │
+    ├── auth/
+    │   ├── bindings/
+    │   │   └── auth_binding.dart
+    │   ├── controllers/
+    │   │   └── auth_controller.dart
+    │   ├── views/
+    │   │   ├── login_screen.dart
+    │   │   └── register_screen.dart
+    │   └── widgets/
+    │       ├── auth_text_field.dart
+    │       └── auth_button.dart
+    │
+    ├── home/
+    │   ├── bindings/
+    │   │   └── home_binding.dart
+    │   ├── controllers/
+    │   │   └── home_controller.dart
+    │   ├── views/
+    │   │   ├── home_screen.dart
+    │   │   ├── mobile_home.dart
+    │   │   ├── tablet_home.dart
+    │   │   └── desktop_home.dart
+    │   └── widgets/
+    │       ├── home_banner.dart
+    │       ├── category_section.dart
+    │       └── trending_products.dart
+    │
+    ├── products/
+    │   ├── bindings/
+    │   │   └── product_binding.dart
+    │   ├── controllers/
+    │   │   └── product_controller.dart
+    │   ├── views/
+    │   │   ├── product_screen.dart
+    │   │   └── product_detail_screen.dart
+    │   └── widgets/
+    │       ├── product_card.dart
+    │       ├── product_grid.dart
+    │       └── product_filter.dart
+    │
+    ├── categories/
+    │   ├── bindings/
+    │   │   └── category_binding.dart
+    │   ├── controllers/
+    │   │   └── category_controller.dart
+    │   ├── views/
+    │   │   ├── category_screen.dart
+    │   │   └── category_products_screen.dart
+    │   └── widgets/
+    │       ├── category_card.dart
+    │       └── category_grid.dart
+    │
+    ├── cart/
+    │   ├── bindings/
+    │   │   └── cart_binding.dart
+    │   ├── controllers/
+    │   │   └── cart_controller.dart
+    │   ├── views/
+    │   │   └── cart_screen.dart
+    │   └── widgets/
+    │       ├── cart_item.dart
+    │       ├── cart_summary.dart
+    │       └── cart_empty.dart
+    │
+    ├── checkout/
+    │   ├── bindings/
+    │   │   └── checkout_binding.dart
+    │   ├── controllers/
+    │   │   └── checkout_controller.dart
+    │   ├── views/
+    │   │   ├── checkout_screen.dart
+    │   │   ├── address_screen.dart
+    │   │   └── payment_screen.dart
+    │   └── widgets/
+    │       ├── address_card.dart
+    │       ├── payment_method.dart
+    │       └── checkout_summary.dart
+    │
+    ├── favorites/
+    │   ├── bindings/
+    │   │   └── favorite_binding.dart
+    │   ├── controllers/
+    │   │   └── favorite_controller.dart
+    │   ├── views/
+    │   │   └── favorite_screen.dart
+    │   └── widgets/
+    │       └── favorite_product_card.dart
+    │
+    ├── orders/
+    │   ├── bindings/
+    │   │   └── order_binding.dart
+    │   ├── controllers/
+    │   │   └── order_controller.dart
+    │   ├── views/
+    │   │   ├── order_screen.dart
+    │   │   └── order_detail_screen.dart
+    │   └── widgets/
+    │       ├── order_card.dart
+    │       └── order_status.dart
+    │
+    └── profile/
+        ├── bindings/
+        │   └── profile_binding.dart
+        ├── controllers/
+        │   └── profile_controller.dart
+        ├── views/
+        │   └── profile_screen.dart
+        └── widgets/
+            ├── profile_header.dart
+            └── profile_menu.dart
